d8b31b08a2aec14fd4587fd18eb993c73b431cc1
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-12-13 21:53:55 -0300

    Create lista1-questão1.cpp

---
+++ lista1-questão1.cpp
@@ -0,0 +1,11 @@
+//Escreva um programa que leia do teclado um caractere e mostre na tela o seu código correspondente em ASCII. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    cout << (int)c << endl;
+    return 0;
+}
