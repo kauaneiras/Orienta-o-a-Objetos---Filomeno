46a427bf458d5d4a68a465f492bb43e8815fbcb6
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-12-14 09:42:25 -0300

    Create lista2-questão01.cpp
    Escreva um programa que mostre a mensagem "Programar torna a
    vida interessante" no écran, utilize uma função mostra para
    escrever a mensagem.

---
+++ lista2-questão01.cpp
@@ -0,0 +1,21 @@
+/*
+Escreva um programa que mostre a mensagem "Programar torna a
+vida interessante" no écran, utilize uma função mostra para
+escrever a mensagem. 
+*/
+
+#include <iostream>
+using namespace std;
+
+void mostra();
+
+int main()
+{
+    mostra();
+    return 0;
+}
+
+void mostra()
+{
+    cout << "Programar torna a vida interessante" << endl;
+}
